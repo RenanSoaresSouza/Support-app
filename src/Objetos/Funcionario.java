c235156fa037eaf301068bed3541869b1e8e2f49
Fratis35 <ciscocarvalhoss@gmail.com>
2026-01-10 14:54:35 -0300

    classe funcionario

---
+++ src/Objetos/Funcionario.java
@@ -0,0 +1,20 @@
+package Objetos;
+
+public class Funcionario extends Pessoa{
+    private int id;
+    private String Usuario;
+    private String Senha;
+
+    public Funcionario(){
+        super();
+    }
+
+    public void verPedidos(){
+
+    }
+  
+    public void concluirPedido(){
+
+    }
+
+}
